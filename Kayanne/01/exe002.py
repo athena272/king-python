bd46f1045cafd45bf88993ecdd00b1241660dbc4
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-12-24 17:15:38 -0300

    Create exe002.py

---
+++ Kayanne/01/exe002.py
@@ -0,0 +1,13 @@
+
+velocity = float(input('Qual a velocidade do carro?: '))
+
+print('zzzzoooommmmmm....')
+
+
+if velocity > 60:
+    print('Você está indo muito rápido, SERÁ MULTADO!!!')
+
+else:
+    print('Dirija com segurança e tenha uma boa viagem!!!')
+
+    
