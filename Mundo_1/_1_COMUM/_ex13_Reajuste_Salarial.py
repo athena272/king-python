6e64243e9c98270c6179426f10f48e39821f00c2
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-02-20 11:54:54 -0300

    Update _ex13_Reajuste_Salarial.py

--- Mundo_1/_1_COMUM/_ex13_Reajuste_Salarial.py
+++ Mundo_1/_1_COMUM/_ex13_Reajuste_Salarial.py
@@ -1,3 +1,3 @@
 salary= float(input('Qual o seu salário atual?: '))
 reajuste_15= salary + (salary * 0.15)
-print('O salário atual de R${} com o reajuste de 15% passará a ser agora de RS{:.2f}'.format(salary, reajuste_15))
+print('O salário atual de R${} com o reajuste de 15% passará a ser agora de RSc'.format(salary, reajuste_15))
