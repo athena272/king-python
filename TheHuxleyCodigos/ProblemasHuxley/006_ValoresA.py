703b41a85a25acd1f0a9a5ebad643915d37c9b4d
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-02-22 02:34:06 -0300

    Create 006_ValoresA.py

---
+++ TheHuxleyCodigos/ProblemasHuxley/006_ValoresA.py
@@ -0,0 +1,12 @@
+contNegativos = 0
+for i in range(0, 5):
+    valor = float(input())
+    if(valor < 0):
+        contNegativos += 1
+
+print('Digite um valor:')
+print('Digite um valor:')
+print('Digite um valor:')
+print('Digite um valor:')
+print('Digite um valor:' )
+print(f'Foram digitados {contNegativos} numeros negativos')
