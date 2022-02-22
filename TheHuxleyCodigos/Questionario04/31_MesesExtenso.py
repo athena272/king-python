ec5facdd79c292c1db351063a13040e94b608fdb
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-02-22 02:36:53 -0300

    Create 31_MesesExtenso.py

---
+++ TheHuxleyCodigos/Questionario04/31_MesesExtenso.py
@@ -0,0 +1,28 @@
+mesAno = int(input())
+#Com base no numero, informa o mes
+if(mesAno == 1):
+    print('janeiro')
+elif(mesAno == 2):
+    print('fevereiro')
+elif(mesAno == 3):
+    print('marco')
+elif(mesAno == 4):
+    print('abril')
+elif(mesAno == 5):
+    print('maio')
+elif(mesAno == 6):
+    print('junho')
+elif(mesAno == 7):
+    print('julho')
+elif(mesAno == 8):
+    print('agosto')
+elif(mesAno == 9):
+    print('setembro')
+elif(mesAno == 10):
+    print('outubro')
+elif(mesAno == 11):
+    print('novembro')
+elif(mesAno == 12):
+    print('dezembro')
+else:
+    print('invalido')
