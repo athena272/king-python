f0f27110f1486a6db6f1cbcab150018108faa537
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-02-22 02:37:02 -0300

    Create 34_ZerinhoUm.py

---
+++ TheHuxleyCodigos/Questionario04/34_ZerinhoUm.py
@@ -0,0 +1,18 @@
+escolhaAlice = int(input())
+escolhaBeto = int(input())
+escolhaClara = int(input())
+               
+aliceGanha = (escolhaAlice != escolhaBeto and escolhaAlice != escolhaClara)
+betoGanha = (escolhaBeto != escolhaAlice and escolhaBeto != escolhaClara)
+claraGanha = (escolhaClara != escolhaAlice and escolhaClara != escolhaBeto)
+
+vencedor = '*'
+
+if (aliceGanha):
+    vencedor = 'A'
+elif(betoGanha):
+    vencedor = 'B'
+elif(claraGanha):
+    vencedor = 'C'
+    
+print(vencedor)
