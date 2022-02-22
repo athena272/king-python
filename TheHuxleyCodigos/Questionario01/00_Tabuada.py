4472b62fa8b7981addbe1a75f8d9f23eae19d67b
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-02-22 02:34:29 -0300

    Create 00_Tabuada.py

---
+++ TheHuxleyCodigos/Questionario01/00_Tabuada.py
@@ -0,0 +1,12 @@
+parcelaConst = int(input()) 
+
+print(parcelaConst, 'X', 1, '=', (parcelaConst * 1))
+print(parcelaConst, 'X', 2, '=', (parcelaConst * 2))
+print(parcelaConst, 'X', 3, '=', (parcelaConst * 3))
+print(parcelaConst, 'X', 4, '=', (parcelaConst * 4))
+print(parcelaConst, 'X', 5, '=', (parcelaConst * 5))
+print(parcelaConst, 'X', 6, '=', (parcelaConst * 6))
+print(parcelaConst, 'X', 7, '=', (parcelaConst * 7))
+print(parcelaConst, 'X', 8, '=', (parcelaConst * 8))
+print(parcelaConst, 'X', 9, '=', (parcelaConst * 9))
+
