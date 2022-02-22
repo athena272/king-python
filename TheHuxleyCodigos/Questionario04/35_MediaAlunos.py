9720ff1cc9a0a8d7e8900f4cbb701197cdd16ef1
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-02-22 02:37:05 -0300

    Create 35_MediaAlunos.py

---
+++ TheHuxleyCodigos/Questionario04/35_MediaAlunos.py
@@ -0,0 +1,14 @@
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+
+media = (nota1 + nota2 + nota3) / 3
+
+situacao = 'prova final'
+
+if(media >= 7):
+    situacao = 'aprovado'
+elif(media < 3):
+    situacao = 'reprovado'
+
+print(situacao)
