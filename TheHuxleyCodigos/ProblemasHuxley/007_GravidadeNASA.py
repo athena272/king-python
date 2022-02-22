346de3521bcd4b2f10334c8d3aa3c7ef1bab07af
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-02-22 02:34:08 -0300

    Create 007_GravidadeNASA.py

---
+++ TheHuxleyCodigos/ProblemasHuxley/007_GravidadeNASA.py
@@ -0,0 +1,25 @@
+pesoTerra = float(input())
+kgTerra = pesoTerra / 9.8
+
+pesoSol = kgTerra * 273.42
+pesoMercurio = kgTerra * 3.78
+pesoVenus = kgTerra * 8.6
+pesoMarte = kgTerra * 3.72
+pesoJupiter = kgTerra * 24.8
+pesoSaturno = kgTerra * 10.5
+pesoUrano = kgTerra * 8.5
+pesoNetuno = kgTerra * 10.8
+pesoPlutao = kgTerra * 5.88
+pesoLua = kgTerra * 1.67
+
+print('%.2f' %pesoSol)
+print('%.2f' %pesoMercurio)
+print('%.2f' %pesoVenus)
+print('%.2f' %pesoTerra)
+print('%.2f' %pesoMarte)
+print('%.2f' %pesoJupiter)
+print('%.2f' %pesoSaturno)
+print('%.2f' %pesoUrano)
+print('%.2f' %pesoNetuno)
+print('%.2f' %pesoPlutao)
+print('%.2f' %pesoLua)
