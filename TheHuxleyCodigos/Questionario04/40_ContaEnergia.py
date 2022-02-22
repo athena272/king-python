bb31c9ad12da8d779ceab72af21a07caf6f392c4
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-02-22 02:37:20 -0300

    Create 40_ContaEnergia.py

---
+++ TheHuxleyCodigos/Questionario04/40_ContaEnergia.py
@@ -0,0 +1,19 @@
+energiaConsumida = int(input())
+
+#Encontrar o preco do KWh
+precoKWh = 1.55
+contaLuz = 35
+
+if(energiaConsumida <= 99):
+    precoKWh = 1.35
+elif(energiaConsumida >= 300 and energiaConsumida <= 574):
+    precoKWh = 1.75
+    energiaConsumida = energiaConsumida + (energiaConsumida * 0.1)#taxa de 10%, pois o consumo esta acima de 300
+elif(energiaConsumida >= 575):
+    precoKWh = 2.15
+    energiaConsumida = energiaConsumida + (energiaConsumida * 0.1)#taxa de 10%, pois o consumo esta acima de 300
+    
+contaLuz = (energiaConsumida * precoKWh)
+
+print('%.2f' %contaLuz)
+print('%.2f' %precoKWh)
