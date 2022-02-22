9bb626293332c1a2d2dd7a117cfb516210b37dc7
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-02-22 02:34:50 -0300

    Create 06_Calculadora.py

---
+++ TheHuxleyCodigos/Questionario01/06_Calculadora.py
@@ -0,0 +1,14 @@
+num1 = float(input())
+num2 = float(input())
+
+#operacaoes com os numeros passados
+soma = (num1 + num2)
+sub = (num1 - num2)
+mult = (num1 * num2)
+div = (num1 / num2)
+
+#Saida calculador
+print('%.2f' %soma)
+print('%.2f' %sub)
+print('%.2f' %mult)
+print('%.2f' %div)
