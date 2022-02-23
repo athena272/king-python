d681db0845af1304222bba1835a3ca6618ccd1b1
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-02-22 23:58:40 -0300

    Create 43_OMaior.py

---
+++ TheHuxleyCodigos/Questionario05/43_OMaior.py
@@ -0,0 +1,16 @@
+#declaracao de funcoes
+def maiorAB(a, b):
+    maior = (a + b + abs(a - b)) // 2
+    return maior
+
+#entrada de dados
+    
+listaValores = input().split()
+
+valor1 = int(listaValores[0])
+valor2 = int(listaValores[1])
+valor3 = int(listaValores[2])
+
+maisMaior = maiorAB((maiorAB(valor1, valor2)), valor3)
+
+print(maisMaior,'eh o maior')
