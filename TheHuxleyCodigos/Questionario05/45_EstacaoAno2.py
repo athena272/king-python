1c1307aded2672b1ed74430c395cb07a773ba195
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-04 21:23:13 -0300

    Update 45_EstacaoAno2.py

--- TheHuxleyCodigos/Questionario05/45_EstacaoAno2.py
+++ TheHuxleyCodigos/Questionario05/45_EstacaoAno2.py
@@ -1,22 +1,22 @@
 def EstacaoAno(hemisferio, dia, mes):
+    estacao = 'PRIMAVERA'
+
     if(hemisferio == 1):
-        if(dia >= 21 and mes == 9) or (dia >= 1 and mes == 10 or mes == 11) or (dia <= 20 and mes == 12):
-            return 'PRIMAVERA'
-        elif(dia >= 21 and mes == 12) or (dia >= 1 and mes == 1 or mes == 2) or (dia <= 20 and mes == 3):
-            return 'VERAO'
+        if(dia >= 21 and mes == 12) or (dia >= 1 and mes == 1 or mes == 2) or (dia <= 20 and mes == 3):
+            estacao = 'VERAO'
         elif(dia >= 21 and mes == 3) or (dia >= 1 and mes == 4 or mes == 5) or (dia <= 20 and mes == 6):
-            return 'OUTONO'
+            estacao = 'OUTONO'
         elif(dia >= 21 and mes == 6) or (dia >= 1 and mes == 7 or mes == 8) or (dia <= 20 and mes == 9):
-            return 'INVERNO'
+            estacao = 'INVERNO'
     else: 
-        if(dia >= 21 and mes == 3) or (dia >= 1 and mes == 4 or mes == 5) or (dia <= 20 and mes == 6):
-            return 'PRIMAVERA'
-        elif(dia >= 21 and mes == 6) or (dia >= 1 and mes == 7 or mes == 8) or (dia <= 20 and mes == 9):
-            return 'VERAO'
+        if(dia >= 21 and mes == 6) or (dia >= 1 and mes == 7 or mes == 8) or (dia <= 20 and mes == 9):
+            estacao = 'VERAO'
         elif(dia >= 21 and mes == 9) or (dia >= 1 and mes == 10 or mes == 11) or (dia <= 20 and mes == 12):
-            return 'OUTONO'
+            estacao = 'OUTONO'
         elif(dia >= 21 and mes == 12) or (dia >= 1 and mes == 1 or mes == 2) or (dia <= 20 and mes == 3):
-            return 'INVERNO'
+            estacao = 'INVERNO'
+    
+    return estacao
 
 hemisferio = int(input())
 dia = int(input())
