5e08ecc2444c3b3b7f82ec25fcf05c320557a9e4
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-04 21:22:46 -0300

    Update 45_EstacaoAno.py

--- TheHuxleyCodigos/Questionario05/45_EstacaoAno.py
+++ TheHuxleyCodigos/Questionario05/45_EstacaoAno.py
@@ -1,13 +1,13 @@
 def EstacaoAno(dia, mes):
-    if(dia >= 21 and mes == 9) or (dia >= 1 and mes == 10 or mes == 11) or (dia <= 20 and mes == 12):
-        return 'PRIMAVERA'
-    elif(dia >= 21 and mes == 12) or (dia >= 1 and mes == 1 or mes == 2) or (dia <= 20 and mes == 3):
-        return 'VERAO'
+    estacao = 'PRIMAVERA'
+    if(dia >= 21 and mes == 12) or (dia >= 1 and mes == 1 or mes == 2) or (dia <= 20 and mes == 3):
+        estacao =  'VERAO'
     elif(dia >= 21 and mes == 3) or (dia >= 1 and mes == 4 or mes == 5) or (dia <= 20 and mes == 6):
-        return 'OUTONO'
+        estacao =  'OUTONO'
     elif(dia >= 21 and mes == 6) or (dia >= 1 and mes == 7 or mes == 8) or (dia <= 20 and mes == 9):
-        return 'INVERNO'
+        estacao = 'INVERNO'
 
+    return estacao
     
 dia = int(input())
 mes = int(input())
