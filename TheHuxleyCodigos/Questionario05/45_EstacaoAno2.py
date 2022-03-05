226487f5a46ef6ad62cbe2bb3c62f7dd81095e00
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-04 21:46:59 -0300

    Update 45_EstacaoAno2.py

--- TheHuxleyCodigos/Questionario05/45_EstacaoAno2.py
+++ TheHuxleyCodigos/Questionario05/45_EstacaoAno2.py
@@ -1,14 +1,14 @@
 def EstacaoAno(hemisferio, dia, mes):
     estacao = 'PRIMAVERA'
 
-    if(hemisferio == 1):
+    if(hemisferio == 1): # hemisferio sul
         if(dia >= 21 and mes == 12) or (dia >= 1 and mes == 1 or mes == 2) or (dia <= 20 and mes == 3):
             estacao = 'VERAO'
         elif(dia >= 21 and mes == 3) or (dia >= 1 and mes == 4 or mes == 5) or (dia <= 20 and mes == 6):
             estacao = 'OUTONO'
         elif(dia >= 21 and mes == 6) or (dia >= 1 and mes == 7 or mes == 8) or (dia <= 20 and mes == 9):
             estacao = 'INVERNO'
-    else: 
+    else: # hemisferio norte
         if(dia >= 21 and mes == 6) or (dia >= 1 and mes == 7 or mes == 8) or (dia <= 20 and mes == 9):
             estacao = 'VERAO'
         elif(dia >= 21 and mes == 9) or (dia >= 1 and mes == 10 or mes == 11) or (dia <= 20 and mes == 12):
