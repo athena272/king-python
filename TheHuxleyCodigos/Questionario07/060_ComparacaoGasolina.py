cf3598ed3a0140a578336b842aebde7c74b057b4
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-15 02:38:53 -0300

    Update 060_ComparacaoGasolina.py

--- TheHuxleyCodigos/Questionario07/060_ComparacaoGasolina.py
+++ TheHuxleyCodigos/Questionario07/060_ComparacaoGasolina.py
@@ -1,12 +1,21 @@
+def maisBarato(gasolinaBR, gasolinaEUA):
+    comparacao = 'Igual'
+    if(gasolinaBR < gasolinaEUA):
+        comparacao = 'Mais barata no Brasil'
+    elif(gasolinaEUA < gasolinaBR):
+        comparacao = 'Mais barata nos EUA'
+
+    return comparacao
+
+
 gasolinaDolar = float(input())
 gasolinaReal = float(input())
 cotacaoDolar = float(input())
 
-dolarToReal =  gasolinaDolar / cotacaoDolar
-galaoToLitro = (dolarToReal * 3.8) 
+dolarToReal = (gasolinaDolar * cotacaoDolar) / 3.8
+quemMaisBarato = maisBarato(gasolinaReal, dolarToReal)
 
 
-print('Gasolina EUA: R$')
+print('Gasolina EUA: R$ %.2f' %dolarToReal)
 print('Gasolina Brasil: R$ %.2f' %gasolinaReal)
-print(galaoToLitro)
-print(dolarToReal)
+print(quemMaisBarato)
