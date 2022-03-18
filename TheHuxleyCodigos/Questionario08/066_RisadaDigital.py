b09409333039daef00b7378da56b2fbf0b375048
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-18 19:08:27 -0300

    Update 066_RisadaDigital.py

--- TheHuxleyCodigos/Questionario08/066_RisadaDigital.py
+++ TheHuxleyCodigos/Questionario08/066_RisadaDigital.py
@@ -1,16 +1,38 @@
+def soVogal(textoRisada):
+    status = True
+    letraAnterior = textoRisada[0]
+    for letra in textoRisada:
+        if(letra not in 'aeiou' or letraAnterior != letra):
+            status = False
+            break
+        letraAnterior = letra    
+    return status
+
+def soConsoante(textoRisada):
+    status = True
+    for letra in textoRisada:
+        if(letra in 'aeiou'):
+            status = False
+            break
+    return status
+
 def risadaInvalida(textoRisada):
     status = False
     maior50Letras = len(textoRisada)
-    if(maior50Letras > 50 or 'aeiou' not in textoRisada):
+    if(maior50Letras > 50 or soConsoante(textoRisada)):
         status = True
     
     return status
 
 
 def risadaEngracada(textoRisada):
-    status = True
-    if('riajkjdhhihhjak' == textoRisada or 'huehuehue' == textoRisada):
-        status = False #Sem graca
+    status = False
+    textoEngracado = ('hahaha' in textoRisada or 'huaauhahhuahau' in textoRisada)
+    textoSemGraca = ('riajkjdhhihhjak' in textoRisada or 'huehuehue' in textoRisada)
+    terSoVogal = soVogal(textoRisada)
+    
+    if((textoEngracado or terSoVogal) and not(textoSemGraca)):
+        status = True #Sem graca
     
     return status
 
@@ -19,11 +41,13 @@ qtdLeituras = int(input())
 
 cont = 0
 while cont < qtdLeituras:
-    textoRisada = input()
-    statusRisada = 'INVALIDA'
+    textoRisada = input().lower()
+    statusRisada = 'SEM GRACA'
     if(risadaEngracada(textoRisada)):
-        print('ENGRACADA')
-    else:
-        print('SEM GRACA')
+        statusRisada = 'ENGRACADA'
+    if(risadaInvalida(textoRisada)):
+        statusRisada = 'INVALIDA'
+    
+    print(statusRisada)
 
     cont += 1
