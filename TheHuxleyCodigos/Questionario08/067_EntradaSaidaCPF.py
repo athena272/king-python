95369d35926f1dcc6b1432d668eb148b6e9e0e84
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-18 23:54:16 -0300

    Create 067_EntradaSaidaCPF.py

---
+++ TheHuxleyCodigos/Questionario08/067_EntradaSaidaCPF.py
@@ -0,0 +1,8 @@
+cpf = input()
+
+# Cada caso de teste contém uma única linha com um CPF no formato XXX.YYY.ZZZ-DD, onde XXX, YYY, ZZZ, DD são números inteiros.
+
+print(cpf[0:3]) #parte XXX do CPF
+print(cpf[4:7]) #parte YYY do CPF
+print(cpf[8:11]) #parte ZZZ do CPF
+print(cpf[12:14]) #parte DD do CPF
