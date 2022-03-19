b91b4814d1099ac579cf5334fc5d3a5762bdb994
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-18 23:54:25 -0300

    Create 071_Palindromo.py

---
+++ TheHuxleyCodigos/Questionario08/071_Palindromo.py
@@ -0,0 +1,18 @@
+def serPalindromo(palavra):
+    status = False
+    palavraInverso = palavra[::-1]
+    if(palavra == palavraInverso):
+        status = True
+    
+    return status
+
+
+qtdLeituras = int(input())
+
+for leituras in range(0, qtdLeituras):
+    palavra = input().lower()
+    
+    resposta = 'NAO'
+    if(serPalindromo(palavra)):
+        resposta = 'SIM'
+    print(resposta)
