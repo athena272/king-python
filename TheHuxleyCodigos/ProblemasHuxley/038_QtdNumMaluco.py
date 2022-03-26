3b65a4603e015cb27f6ccdf4abae0604b65f0601
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-26 01:39:37 -0300

    Create 038_QtdNumMaluco.py

---
+++ TheHuxleyCodigos/ProblemasHuxley/038_QtdNumMaluco.py
@@ -0,0 +1,8 @@
+while True:
+    num = int(input())
+    if num < 0:
+        print('Digite um numero (negativo para parar o programa):')
+        break
+    print('Digite um numero (negativo para parar o programa):')
+    print('Numero:', num)
+   
