ac085a397cee132fe7de42e360ad8a2d1b351ff1
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-26 01:39:41 -0300

    Create 039_QtdDiaMes.py

---
+++ TheHuxleyCodigos/ProblemasHuxley/039_QtdDiaMes.py
@@ -0,0 +1,28 @@
+mesAno = int(input())
+#Com base no numero, informa o mes
+if(mesAno == 1):
+    print('Numero de dias do mes 1  : 31 dias')
+elif(mesAno == 2):
+    print('Numero de dias do mes 2  : 28 dias')
+elif(mesAno == 3):
+    print('Numero de dias do mes 3  : 31 dias')
+elif(mesAno == 4):
+    print('Numero de dias do mes 4  : 30 dias')
+elif(mesAno == 5):
+    print('Numero de dias do mes 5  : 31 dias')
+elif(mesAno == 6):
+    print('Numero de dias do mes 6  : 30 dias')
+elif(mesAno == 7):
+    print('Numero de dias do mes 7  : 31 dias')
+elif(mesAno == 8):
+    print('Numero de dias do mes 8  : 31 dias')
+elif(mesAno == 9):
+    print('Numero de dias do mes 9  : 30 dias')
+elif(mesAno == 10):
+    print('Numero de dias do mes 10  : 31 dias')
+elif(mesAno == 11):
+    print('Numero de dias do mes 11  : 30 dias')
+elif(mesAno == 12):
+    print('Numero de dias do mes 12 : 31 dias')
+else:
+    print('Voce entrou com um mes invalido !')
