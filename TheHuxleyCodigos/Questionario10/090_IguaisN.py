0b1af4594117b589a2f3d56a7b54b20ce2fb5968
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-04-08 23:49:02 -0300

    Create 090_IguaisN.py

---
+++ TheHuxleyCodigos/Questionario10/090_IguaisN.py
@@ -0,0 +1,10 @@
+listaNumeros = list()
+for i in range(0, 101):
+    numero = int(input())
+    listaNumeros.append(numero)
+
+numeroGetIndex = listaNumeros[-1]
+posAparicoes = [indice for indice, numero in enumerate(listaNumeros) if (numero == numeroGetIndex and indice != len(listaNumeros) - 1)]
+
+for pos in posAparicoes:
+    print(pos)
