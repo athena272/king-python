1a50e159e36a22adcc9948c56942f50e6a31e517
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-05-15 11:58:35 -0300

    Create 129_SuprimentoComida.py

---
+++ TheHuxleyCodigos/Questionario15/129_SuprimentoComida.py
@@ -0,0 +1,11 @@
+qtdLeituras = int(input())
+
+for i in range(qtdLeituras):
+    qtdAlimento = float(input())
+    qtdDias = 1
+    while(qtdAlimento / 2 > 1):
+        qtdDias += 1
+        qtdAlimento = qtdAlimento / 2
+    
+    print(qtdDias, 'dias')
+    
