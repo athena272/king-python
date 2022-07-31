aadc988f1e4cb6aa8289869b634b4d6b4ac44286
athena272 <guilhermera272@gmail.com>
2022-07-31 16:11:42 -0300

    Create 002.py

---
+++ BNE/002.py
@@ -0,0 +1,12 @@
+#Link repositorio: https://github.com/athena272/BNE_TI
+
+def Pesquisar(vet, n):
+  if n in vet:
+    print(f"Valor {n} encontrado")
+    print(f"{vet.index(n) + 1}º elemento da lista")
+  else:
+    print(f"Valor {n} nao encontrado")
+    
+Pesquisar([9, 7, 2, 16, 4], 4)
+  
+#Link repositorio: https://github.com/athena272/BNE_TI
