f92e606ba144dd684b8272f21bd5dab27f496e50
athena272 <guilhermera272@gmail.com>
2023-04-09 16:12:06 -0300

    Update 117_FrequenciaCaracter.py

--- TheHuxleyCodigos/Questionario13/117_FrequenciaCaracter.py
+++ TheHuxleyCodigos/Questionario13/117_FrequenciaCaracter.py
@@ -1,11 +1,14 @@
-texto=input()
-aparicoes={}
-listadecaracter=[]
+texto = input()
+aparicoes = {}
+listaCaracter = []
 for letra in texto:
-    aparicoes[letra]=aparicoes.get(letra,0)+1
+    aparicoes[letra] = aparicoes.get(letra,0)+1
+
 for caracter in aparicoes:
-    listadecaracter.append(caracter)
-listadecaracter.sort()
-novalista=listadecaracter[::-1]
+    listaCaracter.append(caracter)
+
+listaCaracter.sort()
+novalista = listaCaracter[::-1]
+
 for elemento in novalista:
     print(elemento,aparicoes[elemento])
